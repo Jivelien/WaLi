dbcb3e43d5a8f793c340a5fda8b020bf995f0ed6
Jivelien <julien.Delannoy@hotmail.com>
2021-09-14 12:52:18 +0200

    get screens directly from xrandr

--- screenwallpaper.py
+++ screenwallpaper.py
@@ -4,6 +4,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def get_screens_information():
+    import os
+    os.system('xrandr --verbose | grep " connected"> screens.txt')
+    
+    xrandr_screens = open('screens.txt', 'rt')
+    screens_list = xrandr_screens.readlines()
+    screens_list = [screen.replace('\n','') for screen in screens_list]
+    screens_list = [screen.replace('(normal left inverted right x axis y axis)','') for screen in screens_list]
+    screens_list = [screen.replace(' x ','x') for screen in screens_list]
+    
+    screens = []
+    
+    for screen_parameters in screens_list:
+        sps = screen_parameters.split()
+    
+        screen_orientation = sps[-2]
+        size_in_mm = tuple([int(elem) for elem in sps[-1].replace('mm','').split('x')])
+        if screen_orientation in ['left', 'right']:
+            size_in_mm = tuple(reversed(size_in_mm))
+            
+        screen_resolution_and_position = sps[-4]
+        screen_resolution_description = screen_resolution_and_position.split('+')[0]
+        screen_resolution = tuple([int(elem) for elem in screen_resolution_description.split('x')])
+        
+        screen_position = tuple([int(elem) for elem in screen_resolution_and_position.split('+')[1:]])
+        
+        screens.append(Ecran(screen_resolution, size_in_mm, screen_position))
+    
+    return screens
+
+    
 #TODO : get xrandr output directly
 # $ xrandr | grep " connected"
 #   eDP-1 connected 1920x1080+1080+1339 (normal left inverted right x axis y axis) 309mm x 173mm
@@ -12,56 +43,57 @@ import matplotlib.pyplot as plt
 #   DVI-I-2-2 connected primary 1920x1080+1080+259 (normal left inverted right x axis y axis) 527mm x 296mm
 #   DVI-I-1-1 connected 1080x1920+3000+0 right (normal left inverted right x axis y axis) 527mm x 296mm
 
-ecran_gauche = Ecran((1080,1920), (296,527), (0,0))
-ecran_milieu = Ecran((1920,1080), (527,296), (1080,259))
-ecran_droite = Ecran((1080,1920), (296,527), (3000,0))
-ecran_bas    = Ecran((1920,1080), (309,173), (1080,1339))
+# ecran_gauche = Ecran((1080,1920), (296,527), (0,0))
+# ecran_milieu = Ecran((1920,1080), (527,296), (1080,259))
+# ecran_droite = Ecran((1080,1920), (296,527), (3000,0))
+# ecran_bas    = Ecran((1920,1080), (309,173), (1080,1339))
 
-ecran_list = [ecran_gauche, ecran_milieu, ecran_droite, ecran_bas]
+# ecran_list = [ecran_gauche, ecran_milieu, ecran_droite, ecran_bas]
 
-max_right_pixel_position = max([ecran.get_right_pixel_position() for ecran in ecran_list])
-max_bottom_pixel_position = max([ecran.get_bottom_pixel_position() for ecran in ecran_list])
+screens = get_screens_information()
+
+max_right_pixel_position = max([ecran.get_right_pixel_position() for ecran in screens])
+max_bottom_pixel_position = max([ecran.get_bottom_pixel_position() for ecran in screens])
+
+
+# def add_screen(array, screen):
+#     array[screen.get_top_pixel_position():screen.get_bottom_pixel_position(),
+#          screen.get_left_pixel_position():screen.get_right_pixel_position()] = 1
+#     return array
+
+# for ecran in screens:
+#     test = add_screen(test, ecran)
+
+# plt.imshow(test)
+# plt.show()
+
+# def add_physical_screen(array, ecran, screen_pixel_per_mm, reference_position = None):
+#     false_pixel_largeur = int(round(ecran.size_in_mm[0] * screen_pixel_per_mm,0))
+#     false_pixel_hauteur = int(round(ecran.size_in_mm[1] * screen_pixel_per_mm ,0))
+#     center = (ecran.get_left_pixel_position() + ecran.get_right_pixel_position()) /2
+#     #print(f"center : {center}")
+#     corrected_top = ecran.get_top_pixel_position() 
+#     corrected_bottom = ecran.get_top_pixel_position() + false_pixel_hauteur
+#     #print(f"hauteur : {corrected_top} - {corrected_bottom} ")
+#     corrected_left = int(round(center - (false_pixel_largeur/2),0)) 
+#     corrected_right = int(round(center + (false_pixel_largeur/2),0))
+#     #print(f"largeur : {corrected_left} - {corrected_right}")
+#     print(f"dim : {corrected_top} : {corrected_bottom}, {corrected_left} : {corrected_right}")
+#     array[corrected_top : corrected_bottom, corrected_left : corrected_right ]  += 1
+#     return array
+
+#TODO find bigger screen as reference
+screen_pixel_per_mm = screens[1].size_in_pixel[1] / screens[1].size_in_mm[1]
 
 test = np.zeros((max_bottom_pixel_position, max_right_pixel_position), dtype='uint8')
-plt.imshow(test)
+# for ecran in screens:
+#     test = add_physical_screen(test, ecran,screen_pixel_per_mm)
 
-def add_screen(array, screen):
-    array[screen.get_top_pixel_position():screen.get_bottom_pixel_position(),
-         screen.get_left_pixel_position():screen.get_right_pixel_position()] = 1
-    return array
-
-for ecran in ecran_list:
-    test = add_screen(test, ecran)
-
-plt.imshow(test)
-
-def add_physical_screen(array, ecran, screen_pixel_per_mm, reference_position = None):
-    false_pixel_largeur = int(round(ecran.size_in_mm[0] * screen_pixel_per_mm,0))
-    false_pixel_hauteur = int(round(ecran.size_in_mm[1] * screen_pixel_per_mm ,0))
-    center = (ecran.get_left_pixel_position() + ecran.get_right_pixel_position()) /2
-    #print(f"center : {center}")
-    corrected_top = ecran.get_top_pixel_position() 
-    corrected_bottom = ecran.get_top_pixel_position() + false_pixel_hauteur
-    #print(f"hauteur : {corrected_top} - {corrected_bottom} ")
-    corrected_left = int(round(center - (false_pixel_largeur/2),0)) 
-    corrected_right = int(round(center + (false_pixel_largeur/2),0))
-    #print(f"largeur : {corrected_left} - {corrected_right}")
-    print(f"dim : {corrected_top} : {corrected_bottom}, {corrected_left} : {corrected_right}")
-    array[corrected_top : corrected_bottom, corrected_left : corrected_right ]  += 1
-    return array
-
-screen_pixel_per_mm = ecran_droite.size_in_pixel[1] / ecran_droite.size_in_mm[1]
-
-for ecran in ecran_list:
-    test = add_physical_screen(test, ecran,screen_pixel_per_mm)
-
-plt.imshow(test)
+# plt.imshow(test)
 
 from PIL import Image, ImageOps
   
-# load the image and convert into
-# numpy array
-
+#TODO : image name in args
 img = Image.open('earth.jpg')
 resized_img = ImageOps.fit(img, (max_right_pixel_position,max_bottom_pixel_position))
 
@@ -85,8 +117,6 @@ def get_correct_right_position(ecran, screen_pixel_per_mm, reference_position = 
     false_pixel_largeur = int(round(ecran.size_in_mm[0] * screen_pixel_per_mm,0))
     center = (ecran.get_left_pixel_position() + ecran.get_right_pixel_position()) /2
     return int(round(center + (false_pixel_largeur/2),0))    
-
-screen_pixel_per_mm = ecran_droite.size_in_pixel[1] / ecran_droite.size_in_mm[1]
 
 left = numpydata[
     get_correct_top_position(ecran_gauche, screen_pixel_per_mm) : 
