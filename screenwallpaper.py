1fc10140606d84ae0b66868bea846068a3b4b65a
Julien <jivelien@gmail.com>
2021-11-26 15:49:51 +0100

    end of poc

--- screenwallpaper.py
+++ screenwallpaper.py
@@ -3,10 +3,9 @@ from ecran import Ecran
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image, ImageOps
-
+import os
 
 def get_screens_information():
-    import os
     os.system('xrandr --verbose | grep " connected"> screens.txt')
     
     xrandr_screens = open('screens.txt', 'rt')
@@ -16,23 +15,25 @@ def get_screens_information():
     screens_list = [screen.replace(' x ','x') for screen in screens_list]
     
     screens = []
-    
     for screen_parameters in screens_list:
-        sps = screen_parameters.split()
-    
-        screen_orientation = sps[-2]
-        size_in_mm = tuple([int(elem) for elem in sps[-1].replace('mm','').split('x')])
-        if screen_orientation in ['left', 'right']:
-            size_in_mm = tuple(reversed(size_in_mm))
+        try:
+            sps = screen_parameters.split()
+            print(sps)
+            screen_orientation = sps[-2]
+            size_in_mm = tuple([int(elem) for elem in sps[-1].replace('mm','').split('x')])
+            if screen_orientation in ['left', 'right']:
+                size_in_mm = tuple(reversed(size_in_mm))
+                
+            screen_resolution_and_position = sps[-4]
+            screen_resolution_description = screen_resolution_and_position.split('+')[0]
+            screen_resolution = tuple([int(elem) for elem in screen_resolution_description.split('x')])
             
-        screen_resolution_and_position = sps[-4]
-        screen_resolution_description = screen_resolution_and_position.split('+')[0]
-        screen_resolution = tuple([int(elem) for elem in screen_resolution_description.split('x')])
-        
-        screen_position = tuple([int(elem) for elem in screen_resolution_and_position.split('+')[1:]])
-        
-        screens.append(Ecran(screen_resolution, size_in_mm, screen_position))
-    
+            screen_position = tuple([int(elem) for elem in screen_resolution_and_position.split('+')[1:]])
+            
+            screens.append(Ecran(screen_resolution, size_in_mm, screen_position))
+        except:
+            pass
+
     return screens
 
 screens = get_screens_information()
@@ -44,11 +45,12 @@ max_bottom_pixel_position = max([ecran.get_bottom_pixel_position() for ecran in 
 screen_pixel_per_mm = screens[1].size_in_pixel[1] / screens[1].size_in_mm[1]
 
   
-#TODO : image name in args
-img = Image.open('zima.jpg')
+import sys
+image_name = sys.argv[1]
+img = Image.open(image_name)
 resized_img = ImageOps.fit(img, (max_right_pixel_position,max_bottom_pixel_position))
 
-numpydata = np.asarray(resized_img)
+numpydata = np.asarray(resized_img)[:,:,:3]
 
 def get_correct_top_position(ecran, screen_pixel_per_mm, reference_position = None):
     return ecran.get_top_pixel_position()
@@ -79,13 +81,17 @@ for screen in screens:
     wallpaper_correct[screen.get_top_pixel_position():screen.get_bottom_pixel_position(),
                       screen.get_left_pixel_position():screen.get_right_pixel_position()] = corrected_img_left
 
+    
 plt.imshow(wallpaper_correct)
 
 wallpaper_pic = Image.fromarray(wallpaper_correct)
 
 import time
 now = int(time.time())
-wallpaper_pic.save(f"test_{now}.jpg")
+wallpaper_pic.save(f"{now}.jpg")
+
+os.system(f'gsettings set org.gnome.desktop.background picture-uri "$(pwd)/{now}.jpg"')
+os.system(f'gsettings set org.gnome.desktop.background picture-options "spanned"')
 
 
 
